77b61e043e00c8365f22d6bcda670d08a3459dc7
Morcinus <30431494+Morcinus@users.noreply.github.com>
2018-10-07 16:59:40 +0200

    Upload DU Lekce 2

---
+++ Lekce 2/DU.java
@@ -0,0 +1,11 @@
+public class DU {
+  public static void main(String[] args){       
+    //Du
+    System.out.println("._____");
+    System.out.println("|\\    \\");
+    System.out.println("| \\ \\\\ \\");
+    System.out.println("|  \\    \\");
+    System.out.println("|   \"\"\"\"\"");
+    System.out.println("`\"\"\"\"\"");
+  }
+}
---
+++ Lekce 2/Prvni.java
@@ -0,0 +1,16 @@
+public class Prvni {
+  public static void main(String[] args){
+  
+    //Ahoj svete
+    System.out.println("Hello world!");
+    System.out.println("Hello \n\"world\"!");
+                
+    //Du
+    System.out.println("._____");
+    System.out.println("|\\    \\");
+    System.out.println("| \\ \\\\ \\");
+    System.out.println("|  \\    \\");
+    System.out.println("|   \"\"\"\"\"");
+    System.out.println("`\"\"\"\"\"");
+  }
+}
