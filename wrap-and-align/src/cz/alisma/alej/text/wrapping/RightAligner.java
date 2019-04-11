3e2fd5f7da8dc4a9c616e02a5dc65861c4b20036
Morcinus <30431494+Morcinus@users.noreply.github.com>
2019-04-11 16:04:16 +0200

    Add files via upload

---
+++ wrap-and-align/src/cz/alisma/alej/text/wrapping/RightAligner.java
@@ -0,0 +1,32 @@
+package cz.alisma.alej.text.wrapping;
+
+import java.util.List;
+
+public class RightAligner implements Aligner{
+	private int width;
+	
+    @Override
+    public String format(List<String> words) {
+        StringBuilder result = new StringBuilder();
+        width = WrapAndAlign.MAX_WIDTH;
+        
+        boolean first = true;
+        for (String w : words) {
+            if (!first) {
+                result.append(" ");
+            } else {
+                first = false;
+            }
+            result.append(w);
+        }
+        
+        int stringLength = result.length();
+        
+        
+        for(int i = 0; i <= width - stringLength; i++) {
+        	result.insert(0, " ");
+        }
+        
+        return result.toString();
+    }
+}
